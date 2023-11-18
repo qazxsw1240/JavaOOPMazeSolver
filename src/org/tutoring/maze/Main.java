fb33f720b19329122292bd79409c1b650714cb9c
qazxsw1240 <qazxsw010528@gmail.com>
2023-11-19 08:13:27 +0900

    update Main.java

--- src/org/tutoring/maze/Main.java
+++ src/org/tutoring/maze/Main.java
@@ -1,8 +1,12 @@
 package org.tutoring.maze;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        MazeGenerator generator = new MazeGenerator(11, 21);
-        System.out.println(generator.createMaze());
+        MazeGenerator generator = new MazeGenerator(51, 51);
+        Maze maze = generator.createMaze();
+        MazeVisualizer visualizer = new MazeVisualizer(List.of(maze));
+        visualizer.start();
     }
 }
